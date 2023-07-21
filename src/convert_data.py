f69b76f996e65a61037ea81c1845b44ec746141a
India Kerle <india.kerle@nesta.org.uk>
2023-07-21 11:43:58 +0100

    add data, pep8 src scripts, add .gitignore file

--- src/convert_data.py
+++ src/convert_data.py
@@ -1,14 +1,14 @@
 """
 Clean up data to:
     - convert cake recipes data to .jsonl format for prodigy
-    - size down cake recipes data 
+    - size down cake recipes data
 
 python src/convert_data.py
 """
 from pathlib import Path
 import pandas as pd
-import json 
-import os 
+import json
+import os
 
 random_state = 43
 data_path = Path.cwd() / 'data'
@@ -20,25 +20,21 @@ cake_recipes = data_path / 'cake_recipes.csv'
 if __name__ == '__main__':
     print(f"formatting {unformatted_training_data} to be in .jsonl...")
     training_data_dict = (pd.read_csv(unformatted_training_data)
-                      #query food related terms 
-                    .query("sentences.str.lower().str.contains('cake|cakes')")
-                    .reset_index()
-                    .rename(columns={'sentences': 'text'})
-                    .drop(columns=["label", "index"])
-                    .to_dict(orient='records'))
-    
-    #dump to jsonl
+                          # query food related terms
+                          .query("sentences.str.lower().str.contains('cake|cakes')")
+                          .reset_index()
+                          .rename(columns={'sentences': 'text'})
+                          .drop(columns=["label", "index"])
+                          .to_dict(orient='records'))
+
+    # dump to jsonl
     with open(data_path / 'training_data.jsonl', 'w') as td:
         for line in training_data_dict:
             json.dump(line, td)
             td.write('\n')
-    
+
     print(f"size down {cake_recipes}...")
     cake_recipes_df = (pd.read_csv(cake_recipes)
                        [['id', 'name', 'RecipeInstructions']]
                        .sample(10, random_state=random_state))
     cake_recipes_df.to_csv(data_path / 'cake_recipes_sample.csv', index=False)
-    
-    
-    
-    
--- src/create_kb.py
+++ src/create_kb.py
@@ -19,20 +19,22 @@ entity_path = data_path / 'cake_recipes_sample.csv'
 
 nlp = spacy.load('en_core_web_sm')
 
-def load_entities(entity_path: Path = entity_path) -> Union[Dict[str, str], Dict[str, str]]:
+
+def load_entities(
+        entity_path: Path = entity_path) -> Union[Dict[str, str], Dict[str, str]]:
     """Load entities from a CSV file.
 
     Args:
         entity_path (Path): Path to the CSV file.
 
     Returns:
-        Union[Dict[str, str], Dict[str, str]]: A dictionary of entity QIDs to entity names 
+        Union[Dict[str, str], Dict[str, str]]: A dictionary of entity QIDs to entity names
             and a dictionary of entity QIDs to entity descriptions.
     """
-        
+
     names = dict()
     descriptions = dict()
-    
+
     with entity_path.open("r", encoding="utf8") as csvfile:
         csvreader = csv.reader(csvfile, delimiter=",")
         for row in csvreader:
@@ -41,13 +43,14 @@ def load_entities(entity_path: Path = entity_path) -> Union[Dict[str, str], Dict
             desc = row[2]
             names[qid] = name
             descriptions[qid] = desc
-            
+
     return names, descriptions
 
-def create_dummy_kb(nlp : spacy.language.Language, 
-                    desc_dict: Dict[str, str], 
-                    name_dict: Dict[str, str] ) -> InMemoryLookupKB:
-    """Create knowledge base from a dictionary of entity QIDs to entity descriptions 
+
+def create_dummy_kb(nlp: spacy.language.Language,
+                    desc_dict: Dict[str, str],
+                    name_dict: Dict[str, str]) -> InMemoryLookupKB:
+    """Create knowledge base from a dictionary of entity QIDs to entity descriptions
         and a dictionary of entity QIDs to entity names.
 
     Args:
@@ -59,35 +62,49 @@ def create_dummy_kb(nlp : spacy.language.Language,
         InMemoryLookupKB: A knowledge base with entities and aliases added.
     """
 
-    #instantiate a knowledge base and add entities
+    # instantiate a knowledge base and add entities
     kb = InMemoryLookupKB(nlp.vocab, entity_vector_length=96)
 
     for qid, desc in desc_dict.items():
         desc_doc = nlp(desc)
         desc_enc = desc_doc.vector
-        kb.add_entity(entity=qid, entity_vector=desc_enc, freq=342)  # 342 is an arbitrary value here
+        kb.add_entity(
+            entity=qid,
+            entity_vector=desc_enc,
+            freq=342)  # 342 is an arbitrary value here
 
     qids = name_dict.keys()
-    probs = [1/(len(qids) + 1) for qid in qids] # to make sure its less than 1 
-    
-    cake_terms = ['cake', 'cakes', '-cakes', 'glazed tarts', 'petrified sponge-cake', 'toasted oat-cake', 'nut cakes', '-cake']
+    probs = [1 / (len(qids) + 1)
+             for qid in qids]  # to make sure its less than 1
+
+    cake_terms = [
+        'cake',
+        'cakes',
+        '-cakes',
+        'glazed tarts',
+        'petrified sponge-cake',
+        'toasted oat-cake',
+        'nut cakes',
+        '-cake']
     for cake_term in cake_terms:
-        kb.add_alias(alias=cake_term, entities=qids, probabilities=probs)  # sum([probs]) should be <= 1 !
-    
+        # sum([probs]) should be <= 1 !
+        kb.add_alias(alias=cake_term, entities=qids, probabilities=probs)
+
     return kb
 
+
 if __name__ == "__main__":
-    
+
     print('loading entities...')
     names, descriptions = load_entities()
     names.pop('id')
     descriptions.pop('id')
-    
+
     print('creating knowledge base...')
     kb = create_dummy_kb(nlp, descriptions, names)
-    
+
     print('saving knowledge base and id to name file ...')
     kb.to_disk(data_path / "cake_kb")
-    #save names description to json to /data folder
+    # save names description to json to /data folder
     with open(data_path / 'id_to_name.json', 'w') as f:
-        json.dump(names, f) 
+        json.dump(names, f)
